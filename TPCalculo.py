dc56f7446067cf71e981bc5a8e4af2ecc08d968a
bautista-gilardon <bautistagilardon@gmail.com>
2023-06-16 10:20:43 -0300

    Pruebas

--- TPCalculo.py
+++ TPCalculo.py
@@ -49,7 +49,7 @@ def circulo(r,x0,y0,N):
 
 #(r,x0,y0) son los datos del circulo, a va de 0 a 100 y representa que porcentaje del circulo se quiere graficar, 
 # y el error es la cantidad de error relativo que se quiere aplicar
-def setDatosCirculares(r,x0,y0,a,error,N):
+def setDatosCirculares(r,x0,y0,a=100,error=0,N=100):
     t = np.linspace(0, 2*np.pi*(a/100), N)
     XY = np.zeros((N,2))
     for i in range(N):
@@ -57,17 +57,14 @@ def setDatosCirculares(r,x0,y0,a,error,N):
         XY[i,1] = (y0 + y0*random.uniform((-error),error)) + (r + r*random.uniform((-error),error))*np.sin(t[i])
     return XY
 
-'''a) datos sobre un circulo completo, sin ruido,
-b) datos sobre un circulo completo, con ruido aleatorio
-c) datos sobre un arco de circunferencia de amplitud 0 < a ≤ 2π, con ruido. ¿Que pasa
-para a chico (del orden de π/4, por ejemplo)?'''
+# a) datos sobre un circulo completo, sin ruido,
+# b) datos sobre un circulo completo, con ruido aleatorio
+# c) datos sobre un arco de circunferencia de amplitud 0 < a ≤ 2π, con ruido.
 
-a = setDatosCirculares(1,0,0,100,0,40)
+a = setDatosCirculares(1,0,0)
 b = setDatosCirculares(1,0,0,100,0.1,150)
-b2 = setDatosCirculares(1,0,0,100,0.8,150)
+b2 = setDatosCirculares(1,0,0,100,0.5,1500)
 c = setDatosCirculares(1,0,0,25,0.1,150)
-
-Sets = [a,b,b2,c]
 
 def compararDatos(datos):
     plt.scatter(datos[:,0], datos[:,1], color='red', s=10)
@@ -78,4 +75,4 @@ def compararDatos(datos):
 
     plt.show()
 
-compararDatos(c)
+compararDatos(b2)
